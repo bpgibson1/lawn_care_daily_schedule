4a3e7680f3f385b917860ab7858278dc052357f3
bpgibson1 <84873880+bpgibson1@users.noreply.github.com>
2021-11-01 19:53:51 -0500

    Initial Commit - Basic class setup

---
+++ daily_schedule.py
@@ -0,0 +1,40 @@
+"""
+**************************************************************
+Name        : daily_schedule.py
+Author      : Bryner Gibson
+Created     : 20211101
+Course      : CIS 152 Data Structures
+Version     : 1.0
+OS          : Windows 10
+Copyright   : This is my own original work based on               
+              specifications issued by our instructor
+Description : This program overall description here
+              Input:  list and describe
+              Output: list and describe
+Academic Honesty: I attest that this is my original work.
+I have not used unauthorized source code, either modified or 
+unmodified. I have not given other fellow student(s) access to
+my program.         
+***************************************************************
+"""
+
+
+class DailySchedule:
+
+    def __init__(self):
+        self.customer_queue = []
+        self.taxes = 0
+        self.surcharge = 0
+        self.flat_fee = 0
+
+    def add_customer(self):
+        pass
+
+    def print_invoice(self):
+        pass
+
+    def sort_customer(self):
+        pass
+
+    def print_schedule(self):
+        pass

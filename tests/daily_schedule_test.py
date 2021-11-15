999bdcb177cc5369b369cb3afab0f30b8a8372e8
Bryner <bpgibson1@dmacc.edu>
2021-11-15 01:45:00 +0000

    update docstring

--- tests/daily_schedule_test.py
+++ tests/daily_schedule_test.py
@@ -10,7 +10,7 @@ Copyright   : This is my own original work based on
               specifications issued by our instructor
 Description : This program overall description here
               Input:  list and describe
-              Output: list and describe
+              Output: tests
 Academic Honesty: I attest that this is my original work.
 I have not used unauthorized source code, either modified or 
 unmodified. I have not given other fellow student(s) access to
